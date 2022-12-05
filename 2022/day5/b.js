006aac32952b5bf9ef6ea626dd3a67117469744b
CB <clint@getkolla.com>
2022-12-05 12:42:35 -0700

    2022 day 5

---
+++ 2022/day5/b.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+var crates = [
+    [],
+    ["B","S","V","Z","G","P","W"],
+    ["J","V","B","C","Z","F"],
+    ["V","L","M","H","N","Z","D","C"],
+    ["L","D","M","Z","P","F","J","B"],
+    ["V","F","C","G","J","B","Q","H"],
+    ["G","F","Q","T","S","L","B"],
+    ["L","G","C","Z","V"],
+    ["N","L","G"],
+    ["J","F","H","C"]
+];
+
+var lines = fs.readFileSync('input.txt').toString().split("\n");
+
+for(i in lines) {
+    words = lines[i].split(" ");
+    count = parseInt(words[1]);
+    from = parseInt(words[3]);
+    to = parseInt(words[5]);
+    //console.log(count, from, to)
+
+    // move count from -> to
+    crates[to] = crates[to].concat(crates[from].splice(-count));
+}
+
+console.log(crates);
+
